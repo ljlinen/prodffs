a7e37c00f9dcc756b0d61d80ec254b30c67fba68
LJLinen <SiphoMoloto0@gmail.com>
2025-02-20 06:42:40 +0200

    new bug found and fied, backageoption was not getting boolean correctly. audio player minimal view also fixed, audio player was leaving the entire view when minimized

--- src/index.jsx
+++ src/index.jsx
@@ -10,8 +10,8 @@ import BeatsContextProvider from "./js/context/BeatsContext";
 import AdminContextProvider from "./js/context/AdminContext";
 import BeatPage from "./js/page/BeatPage";
 
-// export const baseUrl = 'https://prodlinenbackend.linendev.workers.dev'
-export const baseUrl = "http://localhost:8787";
+export const baseUrl = 'https://prodlinenbackend.linendev.workers.dev'
+// export const baseUrl = "http://localhost:8787";
 
 // end of global functions section hopefullu work
 
