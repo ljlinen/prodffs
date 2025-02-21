6bb65d90cb0acb75add0dff82230b6f7359ee269
LJLinen <SiphoMoloto0@gmail.com>
2025-02-21 09:17:09 +0200

    fixing merge conflicts

--- src/js/component/HeaderHome.jsx
+++ src/js/component/HeaderHome.jsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React, { useRef } from 'react'
-import Loader from '../elemet/Loader'
-import ClickableIcon from '../elemet/ClickableIcon'
-import SideNavItem from '../elemet/SideNavItem'
-import iconMenu from "../../asset/img/icon/menu.svg";
-import iconForward from "../../asset/img/icon/chevron-right.svg";
-import iconBack from "../../asset/img/icon/chevron-left.svg";
-import useSideNavigator from '../hooks/useSideNavigator';
-import useBuyingContext from '../hooks/useContext/useBuyingContext';
-import IconButton from './IconButton';
-
-export default function HeaderHome({sorter, setSorter, isBuying, isLoading, setGenre}) {
-   
-   const sideNavReff = useRef(null)
-
-   const { handleSideNav, isSideNavOpen } = useSideNavigator(sideNavReff);
-   const { selectedBeat, buyingDispatch } = useBuyingContext();
-
-
-   const clearBuying = () => {
-      buyingDispatch({
-        type: "CLEAR_BUYING",
-      });
-  
-=======
 import React, { useEffect, useRef, useState } from 'react'
 import Loader from '../elemet/Loader'
 import ClickableIcon from '../elemet/ClickableIcon'
@@ -63,7 +37,6 @@ export default function HeaderHome({sorter, setSorter, resetChechoutInfoCheckout
       if(!resetChechoutInfoCheckout) console.log('resetcheckout is falsy', resetChechoutInfoCheckout);
       
       buyingDispatch({type: "CLEAR_BUYING"});
->>>>>>> client
       // document.body.scrollIntoView({ scrollBehavior: 'smooth' })
    };
 
@@ -71,56 +44,21 @@ export default function HeaderHome({sorter, setSorter, resetChechoutInfoCheckout
    return (
       <div className="header-sidenav">
          <header>
-<<<<<<< HEAD
-            <nav>
-            <IconButton
-              condition={!true}
-              handler={isBuying ? clearBuying : null}
-=======
             <img className='background-img' alt='background' src={headerImage} />
             <nav>
             <IconButton
               condition={true}
               handler={isBuying ? resetChechoutInfoHeader : null}
->>>>>>> client
               src={isBuying ? iconBack : null}
               value={isBuying ? "Back" : "ProdFFS"}
               style={{display: 'flex'}}
             />
-<<<<<<< HEAD
-               <ClickableIcon 
-=======
                {/* <ClickableIcon 
->>>>>>> client
                   condition={isSideNavOpen}
                   classes={'menu'}
                   src={iconMenu}
                   alt={'menu'}
                   handler={() => handleSideNav('Open')}
-<<<<<<< HEAD
-               />
-            </nav>
-            <div className={(isBuying && "beatlist-sorter beatlist-sorter-hide") || "beatlist-sorter"}>
-               <h2>{(isBuying && "BUY BEAT") || "SORTED BY"}</h2>
-               <p className="by">
-                  <span>|</span>
-                  {(selectedBeat?.id && isBuying) || sorter}
-               </p>
-               <div className={(isBuying && " sorter-buttons hide") || "sorter-buttons hide-anim"}>
-                  <p className={sorter === 'newest' && 'current-sorter'} onClick={() => setSorter('newest')}>newest</p>
-                  <p className={sorter === 'plays' && 'current-sorter'} onClick={() => setSorter('plays')}>plays</p>
-                  <div style={{position: 'relative'}}>
-                     <p className={sorter === 'genre' && 'current-sorter'} style={{position: 'absolute'}}>Genre</p>
-                     <select onChange={(e) => setGenre(e.target.value)}  onClick={() => setSorter('genre')} style={{position: 'relative', opacity: 0, width: '100%', height: '100%'}}>
-                        <p>Genre</p>
-                        <option>rap</option>
-                        <option>trap</option>
-                        <option>trap soul</option>
-                     </select>                  
-                  </div>
-
-               </div>
-=======
                /> */}
             </nav>
             <div className={`beatlist-sorter ${isBuying ? 'beatlist-sorter-hide' : null} ${sideNavTab ? 'beatlist-sorter-sidenav' : null }`}>
@@ -146,18 +84,13 @@ export default function HeaderHome({sorter, setSorter, resetChechoutInfoCheckout
                   </div>
                </div>
                {/* <SideNavTabs sideNavTab={sideNavTab} /> */}
->>>>>>> client
             </div>
             <div>
                <div className="">
 
                </div>
             </div>
-<<<<<<< HEAD
-            <Loader load={isLoading} />
-=======
             <Loader isLoading={isLoading} />
->>>>>>> client
          </header>
          <nav className="sidenav" ref={sideNavReff}>
             <ClickableIcon
@@ -169,13 +102,8 @@ export default function HeaderHome({sorter, setSorter, resetChechoutInfoCheckout
                style={{height: 15}}
             />
             <SideNavItem value={'View Cart'} handler={null} />
-<<<<<<< HEAD
-            <SideNavItem value={'Request'} handler={null} />
-            <SideNavItem value={'Contact'} handler={null} />
-=======
             <SideNavItem value={'Request'} handler={() => handleSideNavTabs('request')} />
             <SideNavItem value={'Contact'} handler={() => handleSideNavTabs('contact')} />
->>>>>>> client
          </nav>
       </div>
    )
--- src/js/page/BeatPage.jsx
+++ src/js/page/BeatPage.jsx
@@ -7,34 +7,15 @@ import { baseUrl } from "../..";
 import { useParams } from "react-router-dom";
 import Loader from "../elemet/Loader";
 import useBuyingContext from "../hooks/useContext/useBuyingContext";
-<<<<<<< HEAD
-=======
-import InfoText from "../component/InfoText";
-import AudioPlayer from "../component/AudioPlayer";
-import ButtonDescriptive from "../elemet/ButtonDescriptive";
-import useBeatsContext from "../hooks/useContext/useBeatsContext";
-import Footer from "../component/Footer";
-// import Visualizer from "../component/Visualizer";
->>>>>>> client
 
 export default function BeatPage() {
   const params = useParams();
 
-<<<<<<< HEAD
   const { selectedBeat, buyingDispatch } = useBuyingContext();
 
   const [beat, setBeat] = useState();
   const [isLoading, setIsLoading] = useState();
   const [sorter, setSorter] = useState();
-=======
-  const { selectedBeat } = useBuyingContext();
-  const { beats, beatsDispatch } = useBeatsContext();
-
-  const [beat, setBeat] = useState();
-  const [isLoading, setIsLoading] = useState();
-  const [requestStatusText, setStatusText] = useState()
-  // const [sorter, setSorter] = useState();
->>>>>>> client
 
 
   useEffect(() => {
@@ -47,20 +28,10 @@ export default function BeatPage() {
         if (response.ok) {
           const responseJson = await response.json();
           setBeat(responseJson);
-<<<<<<< HEAD
-=======
-          beatsDispatch({type: 'ADD_BEAT', payload: responseJson})
-        } else {
-          setStatusText(response.statusText);
->>>>>>> client
         }
         setIsLoading(false)
       } catch (error) {
         setIsLoading(false)
-<<<<<<< HEAD
-=======
-        setStatusText(error.message);
->>>>>>> client
         console.log("error loading beatpage", error);
       }
     };
@@ -78,16 +49,9 @@ export default function BeatPage() {
   // useeffect
   useEffect(() => {
     
-<<<<<<< HEAD
   }, [])
 
 
-=======
-  }, [beats])
-
-
-  // eslint-disable-next-line no-unused-vars
->>>>>>> client
   const clearBuying = () => {
 
     // document.body.scrollIntoView({ scrollBehavior: 'smooth' })
@@ -97,11 +61,7 @@ export default function BeatPage() {
     <div className="main-div-beatpage">
       <header>
         <nav>
-<<<<<<< HEAD
           <p className="logo">ProdLinen</p>
-=======
-          <p className="logo">ProdFFS</p>
->>>>>>> client
           <ul>
             <li>Request beat</li>
             <li>Donate</li>
@@ -110,45 +70,6 @@ export default function BeatPage() {
         </nav>
 
         <div className="beatlist-sorter">
-<<<<<<< HEAD
-          <h2 style={{textTransform: 'uppercase'}}>
-            {isLoading ? 'LOADING' : beat ?  beat?.info?.title || "BEAT NOT FOUND" : null}
-            </h2>
-          <p className="by">
-            <span>|</span>
-            { !sorter ? 'select a package below' : sorter + ' package' }
-          </p>
-          <div className="sorter-buttons hide-anim">
-            {
-              beat?.packages ? 
-              Object.values(beat?.packages).map((packageItem, i) => {
-                // if(i === 8) setSorter(packageItem?.package)    11
-                return (
-                  <p className={sorter === packageItem?.package && 'current-sorter'} 
-                    onClick={() => setSorter(packageItem?.package)}
-                    key={i + packageItem?.package}>
-                    {packageItem?.package}
-                  </p>
-                )
-              }) :
-              null
-            }
-          </div>
-        </div>
-      </header>
-
-      <div className="beatlist-main" style={{ minHeight: "92vh" }}>
-        <div className="beatlist">
-          <Loader load={isLoading} />
-
-          <div className="info-text">
-            <h4 style={{ textTransform: "capitalize" }}>
-              take a listen to this beat
-            </h4>
-            <p>if you like it you can proceed to download/buy it.</p>
-          </div>
-
-=======
           <h2>
             {isLoading ? 'LOADING' : beat ?  beat?.info?.title : requestStatusText}
           </h2>
@@ -166,29 +87,12 @@ export default function BeatPage() {
             h4={'download or purchase to this beat'}
             p={'take a listen to ensure you were sent to the right beat'}
           />
->>>>>>> client
 
           { 
             beat ?
               <Beat
                 id={beat.id}
                 beatObj={beat}
-<<<<<<< HEAD
-              /> : 
-            null
-          }
-          
-
-          <div className="page-indicator-bottom">
-            <span className={(sorter === 'newest') && "circle-current"}></span>
-            <span className={(sorter === 'plays') && "circle-current"}></span>
-            <span className={(sorter === 'genre') && "circle-current"}></span>
-          </div>
-
-          <CheckoutPage beatObj={selectedBeat} />
-        </div>
-      </div>
-=======
                 key={beat.id}
                 i={0}
               /> :
@@ -212,7 +116,6 @@ export default function BeatPage() {
         </div>
       </div>
       <Footer />
->>>>>>> client
     </div>
   );
 }
